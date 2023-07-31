670ea3852d52f88994ae2c1f2ef55f9586144732
blang <layup3@naver.com>
2023-07-31 14:03:16 +0900

    useCallback 적용

--- dev-blang-react-pages/src/test3/components/ScheduleBoard.js
+++ dev-blang-react-pages/src/test3/components/ScheduleBoard.js
@@ -2,6 +2,7 @@ import React, { useRef, useState, useCallback } from 'react';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
 
+
 const ScheduleBoard = () => {
 
     const [todos, setTodos] = useState([
@@ -12,36 +13,36 @@ const ScheduleBoard = () => {
 
     const nextId = useRef(todos.length);
 
+    /* (컴포넌트에서 처리하도록 변경. -CTH)
     const list = todos.map((result, index) => {
         return <li id={index}>{result.text}</li>
     });
+    */
 
-    /*********************************************************
-     * useCallback 훅은 함수를 메모이제이션하여 성능 최적화를 위해 사용 
-     * - onInsert 함수는 todos 배열이 변경될 때마다 새로 생성되지 않고 이전에 메모이제이션된 값을 사용합니다.
-     * - 하지만 주의할 점은 nextId.current를 추가하는 것이 좋습니다. 
-     * - nextId.current가 변경되는 경우에도 onInsert 함수가 새로 생성되어야 하기 때문입니다.
-    *********************************************************/
+    /*
+      [useCallBack]: 의존성 배열이 변경되지 않는 한 이전에 생성된 함수를 재사용
+    */
     const onInsert = useCallback( (text) => {
         const temp = {
             id: nextId.current += 1
             , text: text
             , checked: false
-        }
+        };
         setTodos(todos.concat(temp));
     }, [todos]);
 
-    const onRemove = id => {
-        //setTodos(todos.filter(todo => todo.id !== id));
-        setTodos(todos.filter((todo) => todo.id !== id));
-    }
 
-    const onToggle = id => {
+    const onRemove = useCallback( (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    }, [todos]);
+
+
+    const onToggle = useCallback( (id) => {
         setTodos(todos.map(todo =>
             todo.id === id ? { ...todo, checked: !todo.checked } : todo,
         ),
         );
-    }
+    }, [todos]);
 
 
     return (
--- dev-blang-react-pages/src/test3/components/TodoInsert.js
+++ dev-blang-react-pages/src/test3/components/TodoInsert.js
@@ -1,26 +1,44 @@
-import { useState } from "react";
-
+import { useState, useCallBack } from "react";
+import { MdAdd } from 'react-icons/md';
 
 const TodoInsert = ({ onInsert }) => {
 
-  const [text, setText] = useState('');
+  const [value, setValue] = useState('');
 
-  const onChange = (e) => {
+  /* 
+    [useCallBack]: 매번 랜더링 되지 않고 함수 재사용 모드
+  */
+  const onChange = useCallBack( (e) => {
     setText(e.target.value);
-  }
+  }, []);
 
-  const onClick = () => {
-    onInsert(text);
+  /* 삽입 방법1
+  const onClick = useCallBack( () => {
+    onInsert(valued);
     setText('');
-  }
+  }, []);
+  */
+
+  // 삽입 방법2
+  const onSubmit = useCallBack( (e) => {
+    onInsert(value);
+    setValue('');
+    
+    e.preventDefault(); // submit의 새로고침 동작을 막는다.
+  }, [onInsert, value]);
 
   return (
+    <from className="TodoInsert" onSubmit={}>
     <div>
       <b>insert page </b>
-      <input value={text} onChange={onChange} />
-      <button onClick={onClick}>버튼</button>
-
+      <input value={value} onChange={onChange} />
+      {/* <button onClick={onClick}>버튼</button> */}
+      
+      <button type="submit">
+      <MdAdd />
+      </button>
     </div>
+    </from>
   );
 };
 
