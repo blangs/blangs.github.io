38f633a7e76c26e90b2cbf57a7bf8ba33b4659fd
TaeHo Choi <layup3@naver.com>
2023-08-06 17:13:18 +0900

    a

---
+++ dev-blang-react-pages/src/test3/components/immer-v1/ImmerBoard_test.js
@@ -0,0 +1,43 @@
+import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
+
+const ImmerBoard = () => {
+    const nextId = useRef(1);
+    const [form, setForm] = useState({ name: '', username: '' });
+    const [data, setData] = useState({
+        array: [],
+        uselessValue: null
+    });
+
+    
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="username"
+                    placeholder="아이디"
+                    value={form.username}
+                    onChange={onChange}
+                />
+                <input
+                    name="name"
+                    placeholder="이름"
+                    value={form.name}
+                    onChange={onChange}
+                />
+                <button type="submit">등록</button>
+            </form>
+            <div>
+                <ul>
+                    {data.array.map(info => (
+                        <li key={info.id} onClick={() => onRemove(info.id)}>
+                            {info.username} ({info.name})
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default ImmerBoard;
--- dev-blang-react-pages/src/test3/test-main-component3.js
+++ dev-blang-react-pages/src/test3/test-main-component3.js
@@ -3,7 +3,7 @@ import ScheduleBoard1 from './components/schedule-v1/ScheduleBoard';
 import ScheduleBoard2 from './components/schedule-v2/ScheduleBoard';
 import ScheduleBoard3 from './components/schedule-v3/ScheduleBoard';
 import ScheduleBoard4 from './components/schedule-v4/ScheduleBoard'; */
-import ImmerBoard from './components/immer-v1/ImmerBoard';
+import ImmerBoard from './components/immer-v1/ImmerBoard_test';
 
 
 
